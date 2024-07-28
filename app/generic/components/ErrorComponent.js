e4db3a564e22d655d6ae09a933bfd0a74070c574
Sukhjeet Singh Sandhu <sukhjeet.s@codeparva.in>
2024-07-29 02:11:57 +0530

    Readme updated

--- app/generic/components/ErrorComponent.js
+++ app/generic/components/ErrorComponent.js
@@ -1,5 +1,6 @@
 import { memo } from "react";
 import errorStyle from "../styles/ErrorStyle";
+import { Text } from "react-native";
 
 const ErrorComponent = ({ error }) => {
     return <Text style={errorStyle.errorText} >{`${error}`}</Text>;
